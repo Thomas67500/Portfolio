5bbb0ebf48bbc36e933e61a2ee2622086fecc96f
Thomas67500 <thomas.vog67@gmail.com>
2023-08-30 16:00:51 +0200

    implémentation json pour cv et réseau social

---
+++ assets/js/js.js
@@ -0,0 +1,52 @@
+
+
+async function CvThomas () {
+     let response = await fetch ("assets/js/data.json")
+     let data= await response.json()
+     console.log (data)
+     let position = document.getElementById("CV");
+     let anch = document.createElement("a");
+     anch.setAttribute("class","nav-link");
+     anch.href=data[0].CV
+     anch.innerHTML= "Mon CV"
+     position.append(anch)
+
+}
+
+CvThomas()
+
+
+async function Services(){
+    let response = await fetch ("assets/js/data.json")
+    let data= await response.json()
+    
+    
+}
+
+
+
+
+
+    async function Social (){
+        let response = await fetch ("assets/js/data.json")
+     let data= await response.json()
+     console.log (data)
+     let position = document.querySelector(".social-icons");
+     let anchLink= document.createElement("a");
+     anchLink.href="#";
+     anchLink.target= "_blank";
+     anchLink.innerHTML="<i class='lab la-linkedin'></i>"
+    position.append(anchLink);
+
+     let anchGit= document.createElement("a");
+     anchGit.href= data[2].Github;
+     anchGit.target="_blank";
+     anchGit.innerHTML=  "<i class='lab la-github'></i>"
+     position.append(anchGit);
+
+    }
+
+    Social()
+
+
+
